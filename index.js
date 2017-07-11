7644ca9808416859edab7679a5db92af7dabb4a2
Rahul <rkredux@gmail.com>
2017-07-10 20:59:12 -0400

    Change in jQuery AJAX head

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
-  //Default variables
-  var searchQuery, api, data;
 
-  //On Search button click
+  let searchQuery, api, data;
+
+
   $("#search-btn").click(function () {
 
     $(".search-result").empty(); // clears previous results
@@ -12,32 +12,32 @@ $(document).ready(function () {
 
     $.getJSON(api, function(data) {
 
-      //Storing Wikipedia API data which returns arrays inside array
-      var queryTitle = data[1];
-      var queryInfo = data[2];
-      var queryLink = data[3];
+     
+      const queryTitle = data[1], 
+            queryInfo = data[2], 
+            queryLink = data[3];
 
-      //Iritating through arrays
+
       for(var i = 0; i < data[1].length; i++) {
         $(".search-result").append("<li> <a href = " + queryLink[i] + " target='_blank'>" + queryTitle[i] + "</a> <p>" + queryInfo[i] + "</p> </li>");
       }
 
-      //Returns No results
+
       if (data[1].length === 0 && data[2].length === 0 && data[3].length === 0) {
         $(".search-result").append("<li class='no-result'><p>Oops, no results!</p></li>");
       }
     })
 
-    //Push heading up after clicing Search
+ 
     $(".title").animate({"margin-top":"20px","margin-bottom":"20px"});
-    //On smaller devices
+
     var mq = window.matchMedia("(min-width: 575px)");
     if (mq) {
       $(".search-box").animate({"margin-top":"1em","margin-bottom":"1em"});
     }
   });
 
-  //Using Enter key for searching
+
   $("#search-query").keypress(function (e){
       if (e.which === 13) {
           e.preventDefault();
@@ -46,7 +46,7 @@ $(document).ready(function () {
     })
 });
 
-//Random Article button
+
 function randomArticle() {
   window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
 };
